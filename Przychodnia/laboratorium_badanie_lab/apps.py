ee64301b655a06a84326de0a87ad0461159a0a13
rolzwy7 <rolzwy7@gmail.com>
2019-12-28 13:42:33 +0100

    Remove laboratorium_badanie_lab, przychodnia_badanie_fiz apps

--- Przychodnia/laboratorium_badanie_lab/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LaboratoriumBadanieLabConfig(AppConfig):
-    name = 'laboratorium_badanie_lab'
--- Przychodnia/przychodnia_badanie_fiz/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrzychodniaBadanieFizConfig(AppConfig):
-    name = 'przychodnia_badanie_fiz'
